e8c704dbd09e631b7bbac816e01f1ec4877e5ccf
iinfin <han@iinf.in>
2020-12-25 13:11:45 +0300

    Edit homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,19 +10,9 @@ export default function Home() {
       <main>
         <a href="https://discord.gg/b8U5Hdy" className="card">
           <h1>Think Procedural</h1>
-          <p>Discord Server</p>
+          <p>Houdini Discord Community</p>
         </a>
       </main>
-
-      <footer>
-        <a
-          href="https://github.com/thinkprocedural/web"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Source
-        </a>
-      </footer>
 
       <style jsx>
         {`
@@ -46,44 +36,31 @@ export default function Home() {
 
           .card {
             border-radius: 10px;
-            border: 1px solid #f5f5f5;
-            color: inherit;
+            color: #ffffff;
             flex-basis: 45%;
             margin: 1rem;
             padding: 3rem;
             text-align: center;
             text-decoration: none;
-
-            transition: color 200ms ease-in-out, border-color 200ms ease-in-out,
-              box-shadow 200ms ease-in-out;
+            transition: color 200ms ease-in-out;
           }
 
           .card:hover,
           .card:focus,
           .card:active {
-            background-color: #ffffff;
-            box-shadow: 15px 15px 40px #ededed, -15px -15px 40px #ffffff;
-            color: #000000;
+            color: #7f7f7f;
           }
 
           .card h1 {
-            font-size: 3rem;
+            font-size: 5rem;
+            font-weight: 600;
             margin: 0 0 1rem 0;
           }
 
           .card p {
-            color: #1d1d1d;
+            color: #7f7f7f;
             margin: 0;
             padding: 0;
-          }
-
-          footer {
-            align-items: center;
-            border-top: 1px solid #f5f5f5;
-            display: flex;
-            height: 100px;
-            justify-content: center;
-            width: 100%;
           }
         `}
       </style>
