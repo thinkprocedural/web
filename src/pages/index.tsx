b529f0331afee2e4d19757dda7c0424427ab3ce4
iinfin <han@iinf.in>
2021-01-01 12:40:01 +0300

    Remove inline-css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,49 +13,6 @@ export default function Home() {
           <p>Houdini Discord Community</p>
         </a>
       </main>
-
-      <style jsx>
-        {`
-          main {
-            display: flex;
-            flex-direction: column;
-            flex: 1;
-
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-            padding: 0;
-          }
-
-          .card {
-            border-radius: 10px;
-            color: #ffffff;
-            margin: 1rem;
-            padding: 3rem;
-            text-align: center;
-            text-decoration: none;
-            transition: color 200ms ease-in-out;
-          }
-
-          .card:hover,
-          .card:focus,
-          .card:active {
-            color: #7f7f7f;
-          }
-
-          .card h1 {
-            font-size: 5rem;
-            font-weight: 600;
-            margin: 0 0 1rem 0;
-          }
-
-          .card p {
-            color: #7f7f7f;
-            margin: 0;
-            padding: 0;
-          }
-        `}
-      </style>
     </>
   );
 }
