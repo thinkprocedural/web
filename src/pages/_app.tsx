832dd877e5c8a89fe3d9d127159a4de87d07ca3b
iinfin <han@iinf.in>
2021-02-27 18:06:30 +0300

    Add font preload

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import '../styles/index.scss';
-import SEO from '../components/seo';
+import SEO from '../components/_seo';
+import Preload from '../components/_preload';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <SEO />
+      <Preload />
       <Component {...pageProps} />
     </>
   );
